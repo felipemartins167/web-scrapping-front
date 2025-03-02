c5006b22300a8fbbe7ac0ff0370b3871e8be2246
Felipe Martins de Souza <felipemartins167@hotmail.com>
2025-03-02 11:16:02 -0300

    Bugfix Menu

--- fms-adm/src/app/pages/sidebar/sidebar/sidebar.component.ts
+++ fms-adm/src/app/pages/sidebar/sidebar/sidebar.component.ts
@@ -3,16 +3,28 @@ import { RouterModule } from '@angular/router';
 import { Ripple } from 'primeng/ripple';
 import { InputTextModule } from 'primeng/inputtext';
 import { StyleClass } from 'primeng/styleclass';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule, Ripple, InputTextModule, StyleClass],
+  imports: [RouterModule, Ripple, InputTextModule, StyleClass, CommonModule],
   providers: [],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent implements OnInit {
+
+  isSidebarVisible: boolean = false;
+
+  toggleSidebar() {
+    this.isSidebarVisible = !this.isSidebarVisible;
+    if (this.isSidebarVisible) {
+      document.body.classList.add('menu-open');
+    } else {
+      document.body.classList.remove('menu-open');
+    }
+  }
 
   constructor() {}
   
